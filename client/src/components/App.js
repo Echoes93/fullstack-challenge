52a3cf62ec82c6a93f66531a385f7deaae318c01
Dmitry Isachenko <isdmitry93@gmail.com>
2018-11-29 03:14:58 +0300

    FE: rendertest quickfix

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Provider } from "react-redux";
+
 import Upload from "./Upload";
 import Search from "./Search";
+import { store } from "../state/state-store";
+
 
 const App = () => (
-  <div>
-    <Upload />
-    <Search />
-  </div>
+  <Provider store={ store }>
+    <div>
+      <Upload />
+      <Search />
+    </div>
+  </Provider>
 );
 
 export default App;
